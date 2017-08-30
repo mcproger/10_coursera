41bbdfcd4edbddb509a4022418c91e786a66544e
mcproger <artem7lomonosov@gmail.com>
2017-08-30 10:21:01 +0300

    Fix PEP8

--- coursera.py
+++ coursera.py
@@ -8,6 +8,7 @@ def get_courses_list(course_list_page_url):
     course_list = html_content.xpath('//url/loc/text()')
     return course_list[:20]
 
+
 def get_course_info(course_slug):
     pass
 
@@ -19,4 +20,3 @@ def output_courses_info_to_xlsx(filepath):
 if __name__ == '__main__':
     course_list_page_url = 'https://www.coursera.org/sitemap~www~courses.xml'
     print(get_courses_list(course_list_page_url))
-    
