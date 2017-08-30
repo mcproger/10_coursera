015a6a6cf209af83127db5ec975de8eeaca36bfc
mcproger <artem7lomonosov@gmail.com>
2017-08-30 10:18:31 +0300

    Add get_course_list function

--- coursera.py
+++ coursera.py
@@ -1,8 +1,12 @@
+import requests
+from lxml import html
 
 
-def get_courses_list():
-    pass
-
+def get_courses_list(course_list_page):
+    course_list_page = requests.get()
+    html_content = html.fromstring(course_list_page.content)
+    course_list = html_content.xpath('//url/loc/text()')
+    return course_list[:20]
 
 def get_course_info(course_slug):
     pass
@@ -13,4 +17,6 @@ def output_courses_info_to_xlsx(filepath):
 
 
 if __name__ == '__main__':
-    pass
+    course_list_page_url = 'https://www.coursera.org/sitemap~www~courses.xml'
+    print(get_courses_list(course_list_page))
+    
