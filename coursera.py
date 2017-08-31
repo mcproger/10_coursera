e0583ae8159d085c3a6fe0a54dca114ff9e08a20
mcproger <artem7lomonosov@gmail.com>
2017-08-31 15:59:18 +0300

    Add fetching of course title

--- coursera.py
+++ coursera.py
@@ -1,5 +1,6 @@
 import requests
 from lxml import html
+from bs4 import BeautifulSoup
 
 
 def get_courses_list(course_list_page_url):
@@ -19,4 +20,9 @@ def output_courses_info_to_xlsx(filepath):
 
 if __name__ == '__main__':
     course_list_page_url = 'https://www.coursera.org/sitemap~www~courses.xml'
-    print(get_courses_list(course_list_page_url))
+    course_list = get_courses_list(course_list_page_url)
+    for course in course_list:
+        course_page = requests.get(course)
+        soup = BeautifulSoup(course_page.content, 'html.parser')
+        title_of_course = soup.find('h1', class_='title display-3-text').get_text()
+        print(title_of_course)
