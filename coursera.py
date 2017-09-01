e75545009ebfbfbc7af500fd51209170f27dca9b
mcproger <artem7lomonosov@gmail.com>
2017-09-01 18:29:02 +0300

    Upgrade get_course_info function

--- coursera.py
+++ coursera.py
@@ -1,6 +1,7 @@
 import requests
 from lxml import html
 from bs4 import BeautifulSoup
+from openpyxl import Workbook
 
 
 def get_courses_list(course_list_page_url):
@@ -10,28 +11,22 @@ def get_courses_list(course_list_page_url):
     return course_list[:20]
 
 
-def get_html_parse_course_info(course_page, tag_name, class_name):
-    soup = BeautifulSoup(course_page.content, 'html.parser')
-    element = soup.find(tag_name, class_name)
-    if not element:
-        return None
-    return element.get_text()
+def make_course_info_tags(soup):
+    course_info_tags = {
+        'title_of_course': soup.find('h1', class_='title display-3-text'),
+        'language_of_course': soup.find('div', class_='language-info'),
+        'start_of_course': soup.find('div', class_='startdate'),
+        'rating_of_course': soup.find('div', class_='ratings-text bt3-visible-xs')
+    }
+    return course_info_tags
 
 
-def get_course_info(course_list):
-    for course in course_list:
-        course_page = requests.get(course)
-        title_of_course = get_html_parse_course_info(
-            course_page, 'h1', 'title display-3-text')
-        language_of_course = get_html_parse_course_info(
-            course_page, 'div', 'language-info')
-        start_date = get_html_parse_course_info(
-            course_page, 'div', 'startdate')
-        average_course_rating = get_html_parse_course_info(
-            course_page, 'div', 'ratings-text bt3-visible-xs')
-        course_continuance = get_html_parse_course_info(
-            course_page, 'td', 'td-data')
-    return title_of_course, language_of_course, start_date, average_course_rating, course_continuance
+def get_course_info(soup):
+    course_info_tags = make_course_info_tags(soup)
+    course_info = {}
+    for course_tag, course_info_tag in course_info_tags.items():
+        course_info[course_tag] = course_info_tag.get_text() if course_info_tag else None
+    return course_info
 
 
 def output_courses_info_to_xlsx(filepath):
@@ -41,10 +36,11 @@ def output_courses_info_to_xlsx(filepath):
 if __name__ == '__main__':
     course_list_page_url = 'https://www.coursera.org/sitemap~www~courses.xml'
     course_list = get_courses_list(course_list_page_url)
-    course = course_list[0]
-    course_page = requests.get('https://www.coursera.org/learn/missing-data')
-    soup = BeautifulSoup(course_page.content, 'html.parser')
-    
-    course_continuance = soup.find_all('div', class_='week-heading body-2-text')
-    print(len(course_continuance))
-    #print(get_course_info(course_list))
+    courses = []
+    for course in course_list:
+        course_page = requests.get(course)
+        soup = BeautifulSoup(course_page.content, 'html.parser')
+        courses.append(get_course_info(soup))
+    book = Workbook()
+    sheet = book.active
+    book.save("sample.xlsx")
